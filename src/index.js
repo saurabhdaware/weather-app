e51ac2610047f822b491d131fffff48a3ffa477a
S4URABH <30949385+S4URABH@users.noreply.github.com>
2018-06-23 00:45:06 +0530

    data filter to get required data

--- src/index.js
+++ src/index.js
@@ -19,10 +19,46 @@ import axios from 'axios';
         icon:'',
       }
     }
+
+    filterDataWithDate(dataArray){
+      return new Promise(function(resolve,reject){
+        let datesSet = [];
+        let tempHolder;
+        for(let i=0;i<dataArray.length;i++){
+          if(tempHolder === dataArray[i].dt_txt.slice(0,10)){
+            continue;
+          }else{
+            tempHolder = dataArray[i].dt_txt.slice(0,10);
+            datesSet.push(i); 
+            // datesSet.push(dataArray[i].dt_txt.slice(0,10));
+          }
+        }
+        if(datesSet === []){
+          reject('Error occured while getting data');
+        }else{
+          resolve(datesSet);
+        }
+      })
+ 
+    }
+
     render(){
-      axios.get('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=ef5f4331f2a1bbacf3b7f7924e9f9eb7')
+      axios.get('http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=22ea153019299dbbb2ee4028bd3ff603')
       .then((weatherInfo)=>{
-        console.log(weatherInfo.data);
+        this.filterDataWithDate(weatherInfo.data.list)
+        .then((indexes)=>{
+          for(let index of indexes){
+            console.log(weatherInfo.data.list[index]);
+            // This is where everything is going to happen
+          }
+        })
+        .catch((err)=>{
+          console.log(err);
+        });
+
+      })
+      .catch((err)=>{
+        console.log(err);
       })
       return(
         <h1>Content</h1>

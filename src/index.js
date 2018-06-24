f421b8115db53258ddfe76825ad46282c8d26d5c
S4URABH <30949385+S4URABH@users.noreply.github.com>
2018-06-24 17:32:11 +0530

    git pages update

--- src/index.js
+++ src/index.js
@@ -12,15 +12,32 @@ import './index.css';
       );
     }
   }
-
   class TodaysWeather extends React.Component{
-    render(){
-      axios.get('http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=22ea153019299dbbb2ee4028bd3ff603')
-      .then((weatherInfo)=>{
-        
+    constructor(){
+      super();
+      this.state = {
+        name:''
+      }
+      this.renderData();
+    }
+
+    renderData(){
+      navigator.geolocation.getCurrentPosition((location)=>{
+        if(!location.coords){
+          console.log('Location Problem');
+        }else{
+          axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=22ea153019299dbbb2ee4028bd3ff603`)
+          .then((weather)=>{
+            this.setState({name:weather.data.name});
+            console.log(this.state.name);
+          })
+        }
       })
+    }
+
+    render(){
       return (
-        <div className='todaysWeather'><h2>Weather</h2></div>
+        <div className='todaysWeather'><h1>{this.state.name}</h1></div>
       )
     }
   }
@@ -56,28 +73,31 @@ import './index.css';
     }
 
     render(){
-      axios.get('http://api.openweathermap.org/data/2.5/forecast?lat=35&lon=139&appid=22ea153019299dbbb2ee4028bd3ff603')
-      .then((weatherInfo)=>{
-        this.filterDataWithDate(weatherInfo.data.list)
-        .then((indexes)=>{
-          for(let index of indexes){
-            console.log(weatherInfo.data.list[index]);
-            // This is where everything is going to happen
-            // icon = 'http://openweathermap.org/img/w/'+weatherInfo.data.list[index].weather[0].icon+'.png';
-            let img = document.createElement('img');
-            img.src ='http://openweathermap.org/img/w/'+weatherInfo.data.list[index].weather[0].icon+'.png';
-            document.getElementById('icons').appendChild(img);
- 
-          }
+      navigator.geolocation.getCurrentPosition((location)=>{
+        axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=22ea153019299dbbb2ee4028bd3ff603`)
+        .then((weatherInfo)=>{
+          this.filterDataWithDate(weatherInfo.data.list)
+          .then((indexes)=>{
+            for(let index of indexes){
+              console.log(weatherInfo.data.list[index]);
+              // This is where everything is going to happen
+              // icon = 'http://openweathermap.org/img/w/'+weatherInfo.data.list[index].weather[0].icon+'.png';
+              let img = document.createElement('img');
+              img.src ='http://openweathermap.org/img/w/'+weatherInfo.data.list[index].weather[0].icon+'.png';
+              document.getElementById('icons').appendChild(img);
+   
+            }
+          })
+          .catch((err)=>{
+            console.log(err);
+          });
+  
         })
         .catch((err)=>{
           console.log(err);
-        });
+        })
+      })
 
-      })
-      .catch((err)=>{
-        console.log(err);
-      })
       return(
         <div id='icons'></div>
       );

3913078981e8097b65e9bb84cc846449837a970d
S4URABH <30949385+S4URABH@users.noreply.github.com>
2018-06-25 17:15:16 +0530

    Feature update / Today's Weather

--- src/index.js
+++ src/index.js
@@ -6,17 +6,35 @@ import './index.css';
   class Header extends React.Component {
     render() {
       return (
-        <div className='header'>        
-          <TodaysWeather/>
+        <div className='header w3-card-4'>
+        <div className='w3-row'>
+          <div className='w3-col l6 m12 s12'>
+            <div className='todaysWeather-container'>        
+            <TodaysWeather/>
+            </div>
+          </div>
+          <div className='w3-half'>
+          .
+          </div>
+        </div>
         </div>
       );
     }
   }
+
+  
   class TodaysWeather extends React.Component{
     constructor(){
       super();
       this.state = {
-        name:''
+        data:{
+          sys:{},
+          main:{},
+          weather:[{}],
+          wind:{}
+        },
+        ico:'',
+        temp:0
       }
       this.renderData();
     }
@@ -24,12 +42,11 @@ import './index.css';
     renderData(){
       navigator.geolocation.getCurrentPosition((location)=>{
         if(!location.coords){
-          console.log('Location Problem');
+          alert('Location Not Found');
         }else{
           axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=22ea153019299dbbb2ee4028bd3ff603`)
-          .then((weather)=>{
-            this.setState({name:weather.data.name});
-            console.log(this.state.name);
+          .then((weatherInf)=>{
+            this.setState({data:weatherInf.data,ico:`http://openweathermap.org/img/w/${weatherInf.data.weather[0].icon}.png`,temp:parseInt(weatherInf.data.main.temp - 273.15)});
           })
         }
       })
@@ -37,7 +54,20 @@ import './index.css';
 
     render(){
       return (
-        <div className='todaysWeather'><h1>{this.state.name}</h1></div>
+        <div className='todaysWeather w3-card-4'>
+          <img className='todaysWeather-logo' src={this.state.ico} alt='logo' /><br/> 
+          <h1 className='todaysWeather-temp w3-card-4'>{this.state.temp} <small><small><sup><sup>o</sup>C</sup></small></small></h1><br/>
+          <h3>{this.state.data.name}, {this.state.data.sys.country}</h3>
+          <center><hr/></center>
+          <div className='w3-row todaysWeather-moreinfo'>
+            <div className='w3-col'>
+              <h3 className='todaysWeather-mainName'>{this.state.data.weather[0].main} : </h3><h3>{this.state.data.weather[0].description}</h3><br/>
+              <h5>Wind Speed : {this.state.data.wind.speed} m/sec</h5><br/>
+              <h5>Humidity : {this.state.data.main.humidity}%</h5><br/>
+              <h5>Pressure : {this.state.data.main.pressure} hPa</h5><br/>
+            </div>
+          </div>
+        </div>
       )
     }
   }
@@ -83,7 +113,7 @@ import './index.css';
               // This is where everything is going to happen
               // icon = 'http://openweathermap.org/img/w/'+weatherInfo.data.list[index].weather[0].icon+'.png';
               let img = document.createElement('img');
-              img.src ='http://openweathermap.org/img/w/'+weatherInfo.data.list[index].weather[0].icon+'.png';
+              img.src ='http://openweathermap.org/img/w/'+weatherInfo.data.list[index].weather[0].icon.slice(0,2)+'d.png';
               document.getElementById('icons').appendChild(img);
    
             }
